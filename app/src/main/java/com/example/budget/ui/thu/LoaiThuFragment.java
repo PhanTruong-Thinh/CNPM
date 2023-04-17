e47fdebe72f26da83c0a4b4438636188bf781517
Kettsei <110607632+Kettsei@users.noreply.github.com>
2023-04-18 00:19:03 +0700

    p2.full

--- app/src/main/java/com/example/budget/ui/thu/LoaiThuFragment.java
+++ app/src/main/java/com/example/budget/ui/thu/LoaiThuFragment.java
@@ -31,6 +31,11 @@ public class LoaiThuFragment extends Fragment {
         return new LoaiThuFragment();
     }
 
+
+    public LoaiThuViewModel getViewModel() {
+        return mViewModel;
+    }
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
